fab03c5a1416e2508049e261cdba772d875b9cd5
Valdix <novaldix@gmail.com>
2020-10-03 09:43:00 +0300

    рефакт: стиль.правки

--- src/components/add-review-screen/add-review-screen.jsx
+++ src/components/add-review-screen/add-review-screen.jsx
@@ -55,7 +55,7 @@ const AddReviewScreen = () => {
               <input className="rating__input" id="star-3" type="radio" name="rating" value="3" />
               <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-              <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
+              <input className="rating__input" id="star-4" type="radio" name="rating" value="4" defaultChecked={true}/>
               <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
               <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
@@ -64,7 +64,7 @@ const AddReviewScreen = () => {
           </div>
 
           <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" />
             <div className="add-review__submit">
               <button className="add-review__btn" type="submit">Post</button>
             </div>
