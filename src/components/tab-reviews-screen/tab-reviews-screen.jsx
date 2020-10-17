7c173858f001a82a861a691ecb69376ce28ea8c5
Valdix <novaldix@gmail.com>
2020-10-17 09:14:23 +0300

    Рефакт: Удаляет неиспользуемый код в tab-reviews

--- src/components/tab-reviews-screen/tab-reviews-screen.jsx
+++ src/components/tab-reviews-screen/tab-reviews-screen.jsx
@@ -45,38 +45,3 @@ TabReviewsScreen.propTypes = {
 };
 
 export default TabReviewsScreen;
-
-// const TabReviewsScreen = ({reviews}) => {
-
-//   if (reviews.length !== 0) {
-//     const leftColReviews = reviews.filter((review, index) => index % 2 !== 0);
-//     const rightColReviews = reviews.filter((review, index) => index % 2 === 0);
-
-//     return (
-//       <div className="movie-card__reviews movie-card__row">
-//         <div className="movie-card__reviews-col">
-//           {leftColReviews.map((review, index) => (
-//             <ReviewScreen
-//               key={`${review.filmID} - ${index}`}
-//               quote={review.quote}
-//               author={review.author}
-//               datetime={review.datetime}
-//               rating={review.rating}
-//             />
-//           ))}
-//         </div>
-//         <div className="movie-card__reviews-col">
-//           {rightColReviews.map((review, index) => (
-//             <ReviewScreen
-//               key={`${review.filmID} - ${index}`}
-//               quote={review.quote}
-//               author={review.author}
-//               datetime={review.datetime}
-//               rating={review.rating}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     );
-//   }
-// };
